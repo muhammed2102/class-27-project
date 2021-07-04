3c2261a5dfb070fd0ae6ee726527fa9f7181b03a
muhammed2102 <psinan32@gmail.com>
2021-07-04 19:08:51 +0530

    Add platform

--- sketch.js
+++ sketch.js
@@ -21,10 +21,16 @@ function setup() {
 	roofObject=new roof(400,250,230,20);
 	bob1 = new bob(320,575,40)
 	bob2 = new bob(360,575,40)
+	bob3 = new bob(400,575,40)
+	bob4 = new bob(440,575,40)
+	bob5 = new bob(480,575,40)
 	
 	
 	rope1=new rope(bob1.body,roofObject.body,-80, 0)
 	rope2=new rope(bob2.body,roofObject.body,-40, 0)
+	rope3=new rope(bob3.body,roofObject.body,0, 0)
+	rope4=new rope(bob4.body,roofObject.body,40, 0)
+	rope5=new rope(bob5.body,roofObject.body,80, 0)
 	
 	
 	Engine.run(engine);
@@ -40,15 +46,25 @@ function draw() {
 
   rope1.display();
   rope2.display();
+  rope3.display();
+  rope4.display();
+  rope5.display();
 
   bob1.display();
   bob2.display();
+  bob3.display();
+  bob4.display();
+  bob5.display();
   
   
  
 }
 
-
+function keyPressed(){
+	if (keyCode === UP_ARROW) {
+		Matter.Body.applyForce(bob1.body,bob1.body.position,{x:-70,y:-65});
+	}
+}
 
 
 
